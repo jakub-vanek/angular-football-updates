84cbedb9f8ede7a3a96f6ec6fa3c866394905511
Jakub Vaněk <van.niek@seznam.cz>
2023-10-20 10:54:05 +0000

    Working api-key, fixes

--- src/app/services/api-football.service.ts
+++ src/app/services/api-football.service.ts
@@ -7,19 +7,19 @@ import {
   HttpErrorResponse,
   HttpHeaders,
 } from '@angular/common/http';
-import { catchError, EMPTY, map, Observable, throwError } from 'rxjs';
+import { catchError, EMPTY, map, Observable, throwError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiFootballService {
-  private apiUrl: string = 'https://v3.football.api-sports.io';
-  private apiKey: string = '77f7b688d3af90d9b7b0d7b3bd7c68c0';
-  private apiHeaders: HttpHeaders = new HttpHeaders({
+  private apiUrl = 'https://v3.football.api-sports.io';
+  private apiKey = '374c77a9604a3a37c25100f8a818bf71';
+  private apiHeaders = new HttpHeaders({
     'x-rapidapi-host': 'v3.football.api-sports.io',
     'x-rapidapi-key': this.apiKey,
   });
-
+  private resultsToLoad = 10;
   private getLeagueId(country: string): number | undefined {
     switch (country) {
       case 'England':
@@ -49,15 +49,15 @@ export class ApiFootballService {
         .pipe(catchError(this.handleError))
         .pipe(
           map((s: StandingsResponse) => {
-            if (s.errors) {
-              console.error(s.errors);
-              return [];
-            } else if (
+            if (
               s !== undefined &&
-              s.response.length > 1 &&
-              s.response[0].league.standings.length > 1
+              s.response.length > 0 &&
+              s.response[0].league.standings.length > 0
             ) {
               return s.response[0].league.standings[0];
+            } else if (s.errors) {
+              console.error(s.errors);
+              return [];
             } else {
               return [];
             }
@@ -74,17 +74,17 @@ export class ApiFootballService {
       console.log('getStandings', country, season, teamId);
       return this.http
         .get<ResultsResponse>(
-          `${this.apiUrl}/fixtures?team=${teamId}&season=${season}`, // last (????)
+          `${this.apiUrl}/fixtures?team=${teamId}&season=${season}&last=${this.resultsToLoad}`,
           { headers: this.apiHeaders },
         )
         .pipe(catchError(this.handleError))
         .pipe(
           map((s: ResultsResponse) => {
-            if (s.errors) {
+            if (s.response.length > 0) {
+              return s.response;
+            } else {
               console.error(s.errors);
               return [];
-            } else {
-              return s.response;
             }
           }),
         );
