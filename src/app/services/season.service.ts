03f2fb9eb8506485eeb0a863dfa6c2fb0a6be203
Jakub Vaněk <van.niek@seznam.cz>
2023-10-20 11:15:44 +0000

    More accurate season from date

--- src/app/services/season.service.ts
+++ src/app/services/season.service.ts
@@ -5,6 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class SeasonService {
   public getCurrentSeason(): number {
-    return new Date().getFullYear();
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // getMonth() returns 0 based month index
+
+    // This is approximate, as each league and season can start on a different day
+    // If we wanted exact result, we would use the /leagues endpoint to get current season
+    if (month >= 1 && month < 8) {
+      return year - 1;
+    } else {
+      return year;
+    }
   }
 }
